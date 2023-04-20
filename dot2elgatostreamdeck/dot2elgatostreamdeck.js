abcff738d64ff684428020c2f8567a4a9b751728
ArtGateOne <65070703+ArtGateOne@users.noreply.github.com>
2023-04-20 23:20:07 +0200

    1.0.36 Add select bwing
    Now u can set bwing nr in code, or select on boot (default)

--- dot2elgatostreamdeck/dot2elgatostreamdeck.js
+++ dot2elgatostreamdeck/dot2elgatostreamdeck.js
@@ -1,4 +1,4 @@
-//dot2elgatestreamdeck beta v.1.0.2
+//dot2elgatestreamdeck beta v.1.0.36
 
 var W3CWebSocket = require('websocket')
     .w3cwebsocket;
@@ -12,30 +12,35 @@ const { openStreamDeck } = require('@elgato-stream-deck/node');
 
 
 //CONFIG
-var Page = 1;
-var keysend = 0;
-
-; (async () => {
-    const streamDeck = openStreamDeck()
-    await streamDeck.clearPanel()
-
-    const image = await sharp(path.resolve(__dirname, 'fixtures/dot2.png'))
-        .flatten()
-        .resize(streamDeck.ICON_SIZE * streamDeck.KEY_COLUMNS, streamDeck.ICON_SIZE * streamDeck.KEY_ROWS)
-        .raw()
-        .toBuffer()
+var bwing = 0;      //select B-wing 1 or 2, or set 0 - to on boot screen select
+var Page = 1;       //Set Page nr (start)
+var wallpaper = 1;  //Wallpaper 1 = ON, 0 = OFF
+//END-------------------------------
 
 
+if (bwing == 0){
+    //wallpaper = 0;
+    var buttons = [0, 0, 0];
+}
 
-    streamDeck.fillPanelBuffer(image).catch((e) => console.error('Fill failed:', e))
+if (wallpaper == 1){
+    ; (async () => {
+        const streamDeck = openStreamDeck()
+        await streamDeck.clearPanel()
+    
+        const image = await sharp(path.resolve(__dirname, 'fixtures/dot2.png'))
+            .flatten()
+            .resize(streamDeck.ICON_SIZE * streamDeck.KEY_COLUMNS, streamDeck.ICON_SIZE * streamDeck.KEY_ROWS)
+            .raw()
+            .toBuffer()
 
-
-    streamDeck.on('error', (error) => {
-        console.error(error)
-    })
-})()
-
-
+        streamDeck.fillPanelBuffer(image).catch((e) => console.error('Fill failed:', e))
+    
+        streamDeck.on('error', (error) => {
+            console.error(error)
+        })
+    })()
+}
 
 const streamDeck = openStreamDeck();
 streamDeck.clearPanel();
@@ -47,10 +52,18 @@ const imgExecOn = jpegJS.decode(rawExecOn).data;
 const rawExecOff = fs.readFileSync(path.resolve(__dirname, `fixtures/ExecOff_${streamDeck.ICON_SIZE}.jpg`));
 const imgExecOff = jpegJS.decode(rawExecOff).data;
 
+if (bwing == 0){
+    rawButtonBWS = fs.readFileSync(path.resolve(__dirname, `fixtures/selectbwing_${streamDeck.ICON_SIZE}.jpg`));
+    imgButtonBWS = jpegJS.decode(rawButtonBWS).data;
+    rawButtonBW1 = fs.readFileSync(path.resolve(__dirname, `fixtures/bwing1_${streamDeck.ICON_SIZE}.jpg`));
+    imgButtonBW1 = jpegJS.decode(rawButtonBW1).data;
+    rawButtonBW2 = fs.readFileSync(path.resolve(__dirname, `fixtures/bwing2_${streamDeck.ICON_SIZE}.jpg`));
+    imgButtonBW2 = jpegJS.decode(rawButtonBW2).data;
 
-
-
-
+    streamDeck.fillKeyBuffer(0, imgButtonBWS, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+    streamDeck.fillKeyBuffer(1, imgButtonBW1, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+    streamDeck.fillKeyBuffer(2, imgButtonBW2, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+}
 
 var request = -2;
 var session = 0;
@@ -70,47 +83,97 @@ for (i = 0; i < wing; i++) {
     ledmatrix[i] = -1;
 }
 
-
-if (wing == 6) {
-    var buttons = [315, 314, 313, 415, 414, 413];
-} else if (wing == 15) {
-    var buttons = [315, 314, 313, 312, 311, 415, 414, 413, 412, 411, 515, 514, 513, 512, 511];
-} else if (wing == 32) {
-    var buttons = [315, 314, 313, 312, 311, 310, 309, 308, 415, 414, 413, 412, 411, 410, 409, 408, 515, 514, 513, 512, 511, 510, 509, 508, 615, 614, 613, 612, 611, 610, 609, 608];
+if (bwing == 2){
+    if (wing == 6) {
+        var buttons = [315, 314, 313, 415, 414, 413];
+    } else if (wing == 15) {
+        var buttons = [315, 314, 313, 312, 311, 415, 414, 413, 412, 411, 515, 514, 513, 512, 511];
+    } else if (wing == 32) {
+        var buttons = [315, 314, 313, 312, 311, 310, 309, 308, 415, 414, 413, 412, 411, 410, 409, 408, 515, 514, 513, 512, 511, 510, 509, 508, 615, 614, 613, 612, 611, 610, 609, 608];
+    }
+} else if (bwing == 1){
+    if (wing == 6) {
+        var buttons = [307, 306, 305, 407, 406, 405];
+    } else if (wing == 15) {
+        var buttons = [307, 306, 305, 304, 303, 407, 406, 405, 404, 403, 507, 506, 505, 504, 503];
+    } else if (wing == 32) {
+        var buttons = [307, 306, 305, 304, 303, 302, 301, 300, 407, 406, 405, 404, 403, 402, 401, 400, 507, 506, 505, 504, 503, 502, 501, 501, 607, 606, 605, 604, 603, 602, 601, 600];
+    }
 }
 
-
 streamDeck.on('down', (keyIndex) => {
-    client.send('{"requestType":"playbacks_userInput","cmdline":"","execIndex":' + buttons[keyIndex] + ',"pageIndex":' + pageIndex + ',"buttonId":0,"pressed":true,"released":false,"type":0,"session":' + session + ',"maxRequests":0}');
+    if (bwing == 0){
+        //nothing
+    } else {
+        client.send('{"requestType":"playbacks_userInput","cmdline":"","execIndex":' + buttons[keyIndex] + ',"pageIndex":' + pageIndex + ',"buttonId":0,"pressed":true,"released":false,"type":0,"session":' + session + ',"maxRequests":0}');
+    }
+    
 })
 
 streamDeck.on('up', (keyIndex) => {
-    client.send('{"requestType":"playbacks_userInput","cmdline":"","execIndex":' + buttons[keyIndex] + ',"pageIndex":' + pageIndex + ',"buttonId":0,"pressed":false,"released":true,"type":0,"session":' + session + ',"maxRequests":0}');
+    if (bwing == 0){
+        if (keyIndex == 1 || keyIndex == 2){
+            ledmatrix = [-2, -2, -2];
+            console.log("B-wing " + keyIndex);
+            bwing = keyIndex;
+            if (bwing == 2){
+                if (wing == 6) {
+                    buttons = [315, 314, 313, 415, 414, 413];
+                } else if (wing == 15) {
+                    buttons = [315, 314, 313, 312, 311, 415, 414, 413, 412, 411, 515, 514, 513, 512, 511];
+                } else if (wing == 32) {
+                    buttons = [315, 314, 313, 312, 311, 310, 309, 308, 415, 414, 413, 412, 411, 410, 409, 408, 515, 514, 513, 512, 511, 510, 509, 508, 615, 614, 613, 612, 611, 610, 609, 608];
+                }
+            } else if (bwing == 1){
+                if (wing == 6) {
+                    buttons = [307, 306, 305, 407, 406, 405];
+                } else if (wing == 15) {
+                    buttons = [307, 306, 305, 304, 303, 407, 406, 405, 404, 403, 507, 506, 505, 504, 503];
+                } else if (wing == 32) {
+                    buttons = [307, 306, 305, 304, 303, 302, 301, 300, 407, 406, 405, 404, 403, 402, 401, 400, 507, 506, 505, 504, 503, 502, 501, 501, 607, 606, 605, 604, 603, 602, 601, 600];
+                }
+            }
+        }
+    } else {
+        client.send('{"requestType":"playbacks_userInput","cmdline":"","execIndex":' + buttons[keyIndex] + ',"pageIndex":' + pageIndex + ',"buttonId":0,"pressed":false,"released":true,"type":0,"session":' + session + ',"maxRequests":0}');
+    }
 })
 
 streamDeck.on('error', (error) => {
     console.error(error)
 })
 
-
 //sleep 1000
 sleep(1000, function () {
     // executes after one second, and blocks the thread
 });
 
-
 //interval send data to server function
 function interval() {
     if (session > 0) {
 
-        if (wing == 6) {
-            client.send('{"requestType":"playbacks","startIndex":[313,413],"itemsCount":[3,3],"pageIndex":' + pageIndex + ',"itemsType":[3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
-        } else if (wing == 15) {
-            client.send('{"requestType":"playbacks","startIndex":[311,411,511],"itemsCount":[5,5,5],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
-        } else if (wing == 32) {
-            client.send('{"requestType":"playbacks","startIndex":[308,408,508,608],"itemsCount":[8,8,8,8],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+        if (bwing == 2){
+            if (wing == 6) {
+                client.send('{"requestType":"playbacks","startIndex":[313,413],"itemsCount":[3,3],"pageIndex":' + pageIndex + ',"itemsType":[3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else if (wing == 15) {
+                client.send('{"requestType":"playbacks","startIndex":[311,411,511],"itemsCount":[5,5,5],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else if (wing == 32) {
+                client.send('{"requestType":"playbacks","startIndex":[308,408,508,608],"itemsCount":[8,8,8,8],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else {
+                client.send('{"requestType":"playbacks","startIndex":[315],"itemsCount":[1],"pageIndex":' + pageIndex + ',"itemsType":[3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            }
+        } else if (bwing == 1){
+            if (wing == 6) {
+                client.send('{"requestType":"playbacks","startIndex":[305,405],"itemsCount":[3,3],"pageIndex":' + pageIndex + ',"itemsType":[3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else if (wing == 15) {
+                client.send('{"requestType":"playbacks","startIndex":[303,403,503],"itemsCount":[5,5,5],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else if (wing == 32) {
+                client.send('{"requestType":"playbacks","startIndex":[300,400,500,600],"itemsCount":[8,8,8,8],"pageIndex":' + pageIndex + ',"itemsType":[3,3,3,3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            } else {
+                client.send('{"requestType":"playbacks","startIndex":[315],"itemsCount":[1],"pageIndex":' + pageIndex + ',"itemsType":[3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            }
         } else {
-            client.send('{"requestType":"playbacks","startIndex":[315],"itemsCount":[1],"pageIndex":' + pageIndex + ',"itemsType":[3],"view":3,"execButtonViewMode":2,"buttonsViewMode":0,"session":' + session + ',"maxRequests":1}');
+            client.send('{"requestType":"getdata","data":"set","session":' + session + ',"maxRequests":1}');
         }
     }
 }
@@ -171,6 +234,13 @@ client.onmessage = function (e) {
         client.send('{"session":' + session + '}');
         client.send('{"requestType":"getdata","data":"set","session":' + session + ',"maxRequests":1}');
         request = 0;
+        if (bwing == 0){
+            streamDeck.fillKeyBuffer(0, imgButtonBWS, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+            
+            streamDeck.fillKeyBuffer(1, imgButtonBW1, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+            
+            streamDeck.fillKeyBuffer(2, imgButtonBW2, { format: 'rgba' }).catch((e) => console.error('Fill failed:', e));
+        }
     }
 
 
@@ -188,7 +258,7 @@ client.onmessage = function (e) {
         if (obj.forceLogin == true) {
             console.log("LOGIN ...");
             session = (obj.session);
-            client.send('{"requestType":"login","username":"remote","password":"2c18e486683a3db1e645ad8523223b72","session":' + obj.session + ',"maxRequests":10}')
+            client.send('{"requestType":"login","username":"remote","password":"2c18e486683a3db1e645ad8523223b72","session":' + obj.session + ',"maxRequests":10}');
         }
 
         if (obj.session == 0) {
